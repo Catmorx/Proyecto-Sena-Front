c2245f096d53e004909fad9b60f6c21a44aeeaad
catmorx <alismora3@gmail.com>
2025-03-25 11:18:37 -0500

    Primer commit del front

---
+++ assets/js/script.js
@@ -0,0 +1,82 @@
+const toggleMenu = () => {
+  const nav = document.querySelector('.nav-lateral');
+  nav.classList.toggle('expanded');
+}
+
+const addRow = () => {
+  // Obtener los valores de los campos de entrada
+  const item = document.getElementById("item").value;
+  const itemSelect = document.getElementById("item");
+  const itemText = itemSelect.selectedOptions[0].text;
+  const quantity = document.getElementById("quantity").value;
+  const description = document.getElementById("description").value;
+  const rate = parseFloat(document.getElementById("rate").value);
+  const tax = parseFloat(document.getElementById("tax").value);
+
+  if (!item || !description || isNaN(rate) || isNaN(tax) || isNaN(quantity)) {
+    alert("Por favor, complete todos los campos obligatorios.");
+    return;
+  }
+  const amountTotal = rate + (rate * tax / 100);
+
+  // Crear una nueva fila
+  const table = document.getElementById("itemsTable").getElementsByTagName('tbody')[0];
+  const newRow = table.insertRow();
+
+  // Insertar las celdas en la fila
+  const cellItem = newRow.insertCell(0);
+  const cellQuantity = newRow.insertCell(1);
+  const cellDescription = newRow.insertCell(2);
+  const cellRate = newRow.insertCell(3);
+  const cellTax = newRow.insertCell(4);
+  const cellAmountTotal = newRow.insertCell(5);
+  const cellDelete = newRow.insertCell(6);
+
+  // Asignar los valores a cada celda
+  cellItem.textContent = `${item} ${itemText}`;
+  cellQuantity.textContent = quantity;
+  cellDescription.textContent = description;
+  cellRate.textContent = rate.toFixed(2);
+  cellTax.textContent = tax.toFixed(2) + "%";
+  cellAmountTotal.textContent = amountTotal.toFixed(2);
+  const deleteLink = document.createElement("a");
+  deleteLink.className = "a"; 
+  deleteLink.textContent = "Eliminar"; 
+  deleteLink.onclick = () => { deleteRow(this); };
+
+  cellDelete.appendChild(deleteLink);
+  // Limpiar los campos de entrada
+  document.getElementById("item").value = "";
+  document.getElementById("quantity").value = "";
+  document.getElementById("description").value = "";
+  document.getElementById("rate").value = "";
+  document.getElementById("tax").value = "";
+}
+
+const redirectRecord = (bttn) => {
+  const urls = {
+    bttn1: 'salesorder.html',
+    bttn2: 'employee.html',
+    bttn3: 'customer.html',
+    bttn4: 'card.html',
+    bttn5: 'item.html'
+  };
+  const url = urls[bttn]
+  if (url) {
+    window.location.href = url;
+  } else {
+    console.error("No se encontró la URL para el botón con ID:", buttonId);
+  }
+}
+
+const cancelRow = () => {
+  document.getElementById("item").value = "";
+  document.getElementById("quantity").value = "";
+  document.getElementById("description").value = "";
+  document.getElementById("rate").value = "";
+  document.getElementById("tax").value = "";
+}
+const deleteRow = (params) => {
+  const row = params.parentNode.parentNode
+  row.remove();
+}
